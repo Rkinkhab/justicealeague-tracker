3eb03af32935470a9cdf592e3635547dc56de465
Rajat Kinkhabwala <rkinkhab@stevens.edu>
2020-03-30 18:01:11 -0400

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+chmod +x /home/ec2-user/app/target/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/app/scripts/start_server.sh
+chmod +x /home/ec2-user/app/scripts/stop_server.sh
 pkill -f 'java -jar'
